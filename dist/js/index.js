a2961c764f93a2d70a921d0b472e5656f37061e5
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-08-24 11:48:16 +0100

    Advice Generator App FrontEnd Mentor Challenge

---
+++ dist/js/index.js
@@ -0,0 +1,23 @@
+const quoteId = document.querySelector("#advice-id");
+const quote = document.getElementById("advice-quote");
+const btn = document.querySelector("#dice-button");
+const dice = document.getElementsByTagName("img")[1];
+
+btn.addEventListener("click", async function () {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  const { id, advice } = data.slip;
+
+  quote.classList.add("fade-in");
+
+  quote.innerText = ` ${advice} `;
+  quoteId.innerText = id;
+  dice.style.animation = "roll 1s forwards";
+
+  console.log(quote.innerText);
+
+  setTimeout(function () {
+    dice.style.animation = "";
+    quote.classList.remove("fade-in");
+  }, 1000);
+});
